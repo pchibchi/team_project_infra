8d155c8807c062bd92c6ce3363763bc90377e978
pchibchi <hoil15@naver.com>
2026-03-20 15:42:13 +0900

    cluster sg egress 수정

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -37,10 +37,10 @@ resource "aws_security_group" "cluster" {
   vpc_id      = var.vpc_id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 10250
+    to_port         = 10250
+    protocol        = "tcp"
+    security_groups = [aws_security_group.node.id]
   }
 
   tags = merge(
